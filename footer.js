2edcec972d5aa4feb5a0ce35d7e26120cbf02126
Anton Norman <53868993+Airbag65@users.noreply.github.com>
2021-01-20 09:56:50 +0100

    Delete footer.js

--- footer.js
+++
@@ -1,6 +0,0 @@
-const mailAdress1 = document.querySelector(".mailLink").innerHTML;
-
-function copyAdress(){
-    alert(`Kopierade ${mailAdress1} till urklipp!`);
-    document.execCommand("Copy");
-}
